8b0fb4667b08a27834480e3ecad6dbd314d1d1d6
niminonline <niminonline@gmail.com>
2023-09-06 22:32:22 +0530

    project-beta-contents

--- src/app/components/user/user-home-contents/user-home-contents.component.ts
+++ src/app/components/user/user-home-contents/user-home-contents.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectUserData } from 'src/app/core/selectors/user.selectors';
 
 @Component({
   selector: 'app-user-home-contents',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-home-contents.component.scss']
 })
 export class UserHomeContentsComponent {
+
+  constructor(private store:Store){}
+
+  username!:string|undefined;
+
+ngOnInit(){
+  this.store.select(selectUserData).subscribe((response)=>{
+    this.username= response?.username;
+  })
+}
 
 }
